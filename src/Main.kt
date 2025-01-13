3f548a96a5d2ec305472cbecdcf6aa2876876c71
I ne sobaca <sobaca@gmail.com>
2025-01-13 15:09:39 +0900

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,64 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+  //Задание 1
+  println("----- Задание 1 -----")
+  println("-- Типы даннных --"); println("- Целочисленнные типы:");println("Byte: Целое число от -128 до 127 (занимает 1 байт)");println("Short: Целое число от -32768 до 327767 (занимает 2 байта)")
+  println("Int: Целое число от -2147483648(-231) до 2147483647(231-1) (занимает 4 байта)");println("Long: Целое число от -9223372036854775808(-263) до 9223372036854775807(263-1) (занимает 8 байт)")
+  println("- Числа с плавающей точкой:(2 типа)");println("Float: Хранит числа с плавающей точкой от -3.4*1038 до 3.4*1038 (занимает 4 байта)");
+  println("Double: Хранит числа с плавающей точкой от +- 5.0*10-324 до -+1.7*10308(Занимает 8 байт)"); println("- Логический тип Boolean:")
+  println("Boolean: Может хранить одно из двух значений: True или False")
+
+  //Задание 2
+  println("----- Задание 2 -----")
+  val height:Float = 3.14f
+  val num:Double = 6.88
+  println(height)
+  println(num)
+
+    //Задание 3
+  println("----- Задание 3 -----")
+  val e:Double = 27e-1
+  println(e)
+
+  //Задание 4
+  println("----- Задание 4 -----")
+ println("Введите число:")
+  val input = readLine()
+  println("Вы ввели число: $input")
+
+  //Задание 5
+  println("----- Задание 5 -----")
+  println("Введите число:")
+  val input2 = readLine()
+  println("$input2 - вот какое число вы ввели")
+
+  //Задание 6
+  println("----- Задание 6 -----")
+  print(1);print(" ");print(13);print(" ");println(49)
+
+  //Задание 7
+  println("----- Задание 7 -----")
+  print(7);print("  ");print(15);print("  ");println(100)
+
+  //Задание 8
+  println("----- Задание 8 -----")
+  print(29);print("  ");print(17);print("  ");println(42)
+
+  //Задание 9
+  println("----- Задание 9 -----")
+  print(46);print(" ");print(127);print(" ");print(28);print(" ");println(43)
+
+  //Задание 10
+  println("----- Задание 10 -----")
+  println(50);println(10)
+
+  //Задание 11
+  println("----- Задание 11 -----")
+  println(5);println(10);println(21)
+
+  //Задание 12
+  println("----- Задание 12 -----")
+  println(4);println(26);println(150);println(111)
+}
+
